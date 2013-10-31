a99d81d667fb761bc19b8597a11fee9ad4abcd35
Erlend Oftedal <erlend@oftedal.no>
2013-10-31 22:06:42 +0100

    Allow local repo caches to be used. Fix jQuery migrate detection

--- node/lib/repo.js
+++ node/lib/repo.js
@@ -1,5 +1,6 @@
 /* global require, console, exports */
-var http = require('https'),
+var http   = require('https'),
+    fs     = require('fs'),
 	retire = require('./retire');
 var emitter = require('events').EventEmitter;
 
@@ -22,9 +23,20 @@ function loadJson(url) {
 	});
 	return events;
 }
-
+function loadJsonFromFile(file) {
+    console.log('Reading ' + file + ' ...');
+	var events = new emitter();
+    fs.readFile(file, {}, function(err, data) {
+      var obj = JSON.parse(retire.replaceVersion(''+data));
+      events.emit('done', obj);
+    });
+    return events;
+}
 
 
 exports.loadrepository = function(repoUrl) {
 	return loadJson(repoUrl);
+};
+exports.loadrepositoryFromFile = function(filepath) {
+	return loadJsonFromFile(filepath);
 };
