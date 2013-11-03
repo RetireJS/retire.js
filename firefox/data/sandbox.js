356f638e88f40d0abe45b8ab869d1d7f7db13482
Thomas Andersen <thomas@mr-andersen.no>
2013-11-03 17:33:27 +0100

    - Sandbox solution, work in progress - Toggle developer tools/console when retire button is clicked

--- firefox/data/sandbox.js
+++ firefox/data/sandbox.js
@@ -1,17 +1,101 @@
-self.port.on("testScript", function (script, repoFuncs) {
-    try {
-        // For some reason http://erlend.oftedal.no/blog/retire/dojo.js fails when eval'd
-        eval(script);
-    	for(var component in repoFuncs) {
-        	repoFuncs[component].forEach(function(func) {
-            	try {
-                	var result = eval(func);
-                	self.port.emit("result", { component : component, version : result })
+self.port.on("detect-version", function (script, repoFuncs) {
 
-            	} catch(e) {
-            	}
-        	});
-    	}
-	} catch(e) {
-	}
+  console.log("sandbox.js - detect-version");
+
+  // Solution 1
+  // Load the script in a script element and try the repo functions in order to find a version string.
+  // Works, but seems hacky and creates leaks
+  // Problem: the page-worker's destroy method does not remove the background window object causing the add-on to leak (see lib/main, line 170).
+  // The GC will not remove the window probably because objects are still alive in the sanbox.html document.
+  // I have not found a working way to clean up the added script's/object.
+  // We could make the page-worker global in the add-on to avoid all the background pages, but then the background page will pile up with scripts and
+  // The page needs to be 'fresh' each time a script is added in order to not create conflicts between scripts etc.
+
+  /**
+   * Since the dom can be modified by a script, content scripts transparently access the original dom values in something called XRayWrapper.
+   * The actual dom lives in window.unsafeWindow so we need to look there to find user objects.
+   * Since 'window' has cyclic values we need to get the visible values only, get the unsafeWindow reference and run our eval inside the unsafeWindow.
+   * 
+   * More information about this here:
+   * https://addons.mozilla.org/en-US/developers/docs/sdk/latest/dev-guide/guides/content-scripts/accessing-the-dom.html
+   * https://bugzilla.mozilla.org/show_bug.cgi?id=787070
+   * https://bugzilla.mozilla.org/show_bug.cgi?id=787013
+   * https://bugzilla.mozilla.org/show_bug.cgi?id=934296
+   */
+      
+  var scriptEl = document.createElement("script");
+  scriptEl.setAttribute("type", "text/javascript");
+  scriptEl.text = script;
+  document.head.appendChild(scriptEl);
+
+  // Hack, the following gets the unsafeWindow where user added objects lives and runs the test in unsafeWindow
+  var visible = [];
+  var windowAsString = JSON.stringify(window, function(key, val) {
+    if (typeof val == "object") {
+      if (visible.indexOf(val) >= 0) {
+        return;
+      }
+      visible.push(val);
+    }
+    return val;
+  });
+  var unsafeWindow = JSON.parse(windowAsString).unsafeWindow;
+  with(unsafeWindow) {
+    for(var component in repoFuncs) {
+      repoFuncs[component].forEach(function(func) {
+        try {
+          var result = eval(func);
+          self.port.emit("version-detected", { component : component, version : result })
+        } catch(e) {
+        }
+      });
+    }
+  }
+  
+  /*  
+  // Solution 2
+  // Create a new Iframe and load the script in the iframe's window
+  // Problem: Can not access the window. TypeError: window/contentWindow is a cyclic object value
+  
+  var head = document.getElementsByTagName("head")[0];
+  var iframe = document.createElement("iframe");
+
+  iframe.onload = function () {
+    var scriptEl = iframe.contentDocument.createElement("script");
+    scriptEl.setAttribute("type", "text/javascript");
+    scriptEl.text = script;
+    iframe.contentDocument.getElementsByTagName('head')[0].appendChild(scriptEl);
+    console.log(iframe.contentWindow); // TypeError: object is a cyclic object value
+    console.log(iframe.contentWindow.wrappedJSObject);
+  };
+  document.body.appendChild(iframe);
+  */
+  
+  /*
+  // Solution 3
+  // eval the script/text blob, try finding the library version
+  // This works in chrome background scripts
+  // The problem with this is that libraries sometimes contains detector code accessing 'window' object and what not
+  // that is problematic running in a content/xray document ?
+  // (eval() added before the try/catch to better see the exceptions)
+  eval(script);
+  try {
+    // For some reason the following scripts fails fails when eval'd
+    // http://erlend.oftedal.no/blog/retire/dojo.js
+    // http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.3.3/backbone-min.js
+
+    for(var component in repoFuncs) {
+      repoFuncs[component].forEach(function(func) {
+        try {
+          var result = eval(func);
+          self.port.emit("version-detected", { component : component, version : result })
+        } catch(e) {}
+      });
+    }
+  } catch(e) {}
+  */
+  
+  // Notify the add-on so it can destroy the pageWorker instance
+  self.port.emit("done");
+  
 });
--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -11,222 +11,226 @@ const tabs = require("sdk/tabs");
 const windowUtil = require("sdk/window/utils");
 const tabUtil = require("sdk/tabs/utils");
 const URL = require("sdk/url").URL;
-
-var tabMap = new Map();
-var eventTarget = {};
+const worker = require("sdk/page-worker");
 
 var repoUrl = "https://raw.github.com/bekk/retire.js/master/repository/jsrepository.json";
-
 var updatedAt = Date.now();
 var repo;
 var repoFuncs;
 var cache = [];
 var vulnerable = {};
+var tabMap = new Map();
+var eventTarget = {};
 
-// Install 
-var tbb = toolbarButton({
-    id: "retire-js",
-    label: "retire.js",
-    tooltiptext: "retirejs",
-    image: data.url("icons/icon16.png")
+var button = toolbarButton({
+  id: "retire-js",
+  label: "retire.js",
+  tooltiptext: "retirejs",
+  image: data.url("icons/icon16.png"),
+  onCommand: () => {
+    let tabBrowser = windowUtil.getMostRecentBrowserWindow().gBrowser;
+    windowUtil.getMostRecentBrowserWindow().gDevToolsBrowser.selectToolCommand(tabBrowser, "webconsole");
+  }
 });
-tbb.moveTo({
-    toolbarID: 'nav-bar',
-    forceMove: false
+button.moveTo({
+  toolbarID: 'nav-bar',
+  forceMove: false
 });
 
-// Tab activity
 tabs.on("activate", (tab) => {
-    if (tabMap.get(tab.id)) {
-        setBadgeCount(tabMap.get(tab.id).fileCount);
-    } else {
-        setBadgeCount("");
-    }
+  if (tabMap.get(tab.id)) {
+    setBadgeCount(tabMap.get(tab.id).fileCount);
+  } else {
+    setBadgeCount("");
+  }
 });
 tabs.on("close", (tab) => {
-    delete tabMap.delete(tab.id);
+  delete tabMap.delete(tab.id);
 });
 
 function download(url) {
-	var deferred = promise.defer();
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if (xhr.status == 200) {
-				deferred.resolve(xhr.responseText);
-			} else {
-				console.warn("Got " + xhr.status + " when trying to download " + url);
-			}
-		}
-	};
-	xhr.open("GET", url, true);
-	xhr.send();
-	return deferred.promise;
+  var deferred = promise.defer();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        deferred.resolve(xhr.responseText);
+      } else {
+        console.warn("Got " + xhr.status + " when trying to download " + url);
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+  return deferred.promise;
 }
 
 function downloadRepo() {
-	var deferred = promise.defer();
-    console.log("Downloading repo ...");
-    updatedAt = Date.now();
-    download(repoUrl + "?" + updatedAt).then((repoData) => {
-        repo = JSON.parse(retire.replaceVersion(repoData));
-        cache = [];
-        vulnerable = {};
-        setFuncs();
-    	deferred.resolve();
-        console.log("Repo downloaded");
-    });
-    return deferred.promise;
+  var deferred = promise.defer();
+  console.log("Downloading repo ...");
+  updatedAt = Date.now();
+  download(repoUrl + "?" + updatedAt).then((repoData) => {
+    repo = JSON.parse(retire.replaceVersion(repoData));
+    cache = [];
+    vulnerable = {};
+    setFuncs();
+    deferred.resolve();
+    console.log("Repo downloaded");
+  });
+  return deferred.promise;
 }
 
 function setFuncs() {
-    repoFuncs = {};
-    for (var component in repo) {
-        if (repo[component].extractors.func) {
-            repoFuncs[component] = repo[component].extractors.func;
-        }
+  repoFuncs = {};
+  for (var component in repo) {
+    if (repo[component].extractors.func) {
+      repoFuncs[component] = repo[component].extractors.func;
     }
+  }
 }
 
 function getFileName(url) {
-    return new URL(url).path;
+  return new URL(url).path;
 }
 
 function setBadgeCount(count) {
-    tbb.badge = {
-        text: Number(count) > 0 ? count : "",
-        color: 'rgb(230, 96, 0)'
-    }
+  button.badge = {
+    text: Number(count) > 0 ? count : "",
+    color: 'rgb(193, 56, 50)'
+  }
 }
 
 function getWindowForRequest(request){
-    if (request instanceof Ci.nsIRequest) {
-        try{
-            if (request.notificationCallbacks) {
-                return request.notificationCallbacks.getInterface(Ci.nsILoadContext).associatedWindow;
-            }
-        } catch(e) {
-        }
-        try {
-            if (request.loadGroup && request.loadGroup.notificationCallbacks) {
-                return request.loadGroup.notificationCallbacks.getInterface(Ci.nsILoadContext).associatedWindow;
-            }
-        } catch(e) {
-        }
+  if (request instanceof Ci.nsIRequest) {
+    try {
+      if (request.notificationCallbacks) {
+        return request.notificationCallbacks.getInterface(Ci.nsILoadContext).associatedWindow;
+      }
+    } catch(e) {
     }
-    return null;
+    try {
+      if (request.loadGroup && request.loadGroup.notificationCallbacks) {
+        return request.loadGroup.notificationCallbacks.getInterface(Ci.nsILoadContext).associatedWindow;
+      }
+    } catch(e) {
+    }
+  }
+  return null;
+}
+
+function isChannelInitialDocument(httpChannel) {
+  return httpChannel.loadFlags & httpChannel.LOAD_INITIAL_DOCUMENT_URI;
 }
 
 events.on(eventTarget, "scan", (details) => {
-	//console.log("*********** SCAN ***********");
-    if ((Date.now() - updatedAt) > 1000*60*60*6) {
-        downloadRepo().then(() => { 
-            events.emit(eventTarget, "scan", details); 
-        });
-        return;
-    }
-    if (cache.indexOf(details.url) > -1) {
-        if (vulnerable.hasOwnProperty(details.url)) {
-            events.emit(eventTarget, "result-ready", details, vulnerable[details.url]);
-        }
-        return;
-    }
-    cache.push(details.url);
-    //console.log("Scanning " + details.url + " ...");
-    var results = retire.scanUri(details.url, repo);
-    console.log("scan uri: " + results.length);
-    if (results.length > 0) {
-        events.emit(eventTarget,'result-ready', details, results);
-        return;
-    }
-    results = retire.scanFileName(getFileName(details.url), repo);
-    console.log("scan filename: " + results.length);
-    if (results.length > 0) {
-        events.emit(eventTarget,'result-ready', details, results);
-        return;
-    }
-    download(details.url).then((content) => { 
-        events.emit(eventTarget,'script-downloaded', details, content); 
+  if ((Date.now() - updatedAt) > 1000*60*60*6) {
+    downloadRepo().then(() => { 
+      events.emit(eventTarget, "scan", details); 
     });
     return;
+  }
+  if (cache.indexOf(details.url) > -1) {
+    if (vulnerable.hasOwnProperty(details.url)) {
+      events.emit(eventTarget, "result-ready", details, vulnerable[details.url]);
+    }
+    return;
+  }
+  cache.push(details.url);
+
+  var results = retire.scanUri(details.url, repo);
+  console.log("scanUri: " + results.length + ", "  + details.url);
+  if (results.length > 0) {
+    events.emit(eventTarget, "result-ready", details, results);
+    return;
+  }
+
+  results = retire.scanFileName(getFileName(details.url), repo);
+  console.log("scanFileName: " + results.length + ", "  + details.url);
+  if (results.length > 0) {
+    events.emit(eventTarget, "result-ready", details, results);
+    return;
+  }
+  download(details.url).then((content) => { 
+    events.emit(eventTarget, "script-downloaded", details, content); 
+  });
+  return;
 });
 
 events.on(eventTarget, "script-downloaded", (details, content) => {
-    //fixme: buggy. For some reason the "dojo.js" file reports an error: console[_8] is not defined when eval'ed
-
-    var results = retire.scanFileContent(content, repo, hasher);
-    //console.log("script-downloaded, results: " + results.length + ", "  + details.url);
-    if (results.length > 0) {
-        events.emit(eventTarget, "result-ready", details, results);
-        return;
-    }
-
-    events.emit(eventTarget, "sandbox", details, content);
+  var results = retire.scanFileContent(content, repo, hasher);
+  console.log("scanFileContent: " + results.length + ", "  + details.url);
+  if (results.length > 0) {
+    events.emit(eventTarget, "result-ready", details, results);
+    return;
+  }
+  events.emit(eventTarget, "sandbox", details, content);
 });
 
 events.on(eventTarget, "sandbox", (details, content) => {
-    var pageWorker = require("sdk/page-worker").Page({
-        contentScriptFile: data.url("sandbox.js"),
-        contentURL: data.url("sandbox.html"),
-        contentScriptWhen: "end"
-    });
-    pageWorker.port.emit("testScript", content, repoFuncs);
-    pageWorker.port.on("result", (result) => {
-        if (result.version) {
-            var results = retire.check(result.component, result.version, repo);
-
-            events.emit(eventTarget, "result-ready", details, results);
-            //events.emit(eventTarget, "result-ready", { url : evt.data.original.url, tabId : evt.data.original.tabId }, results);
-        }
-    });
+  var pageWorker = worker.Page({
+    contentScriptFile: data.url("sandbox.js"),
+    contentURL: data.url("sandbox.html"),
+    contentScriptWhen: "end"
+  });
+  pageWorker.port.emit("detect-version", content, repoFuncs);
+  pageWorker.port.on("version-detected", (result) => {
+    if (result.version) {
+      var results = retire.check(result.component, result.version, repo);
+      console.log("version-detected, results: " + results.length + ", "  + details.url);
+      events.emit(eventTarget, "result-ready", details, results);
+    }
+  });
+  pageWorker.port.on("done", () => {
+    pageWorker.destroy();
+  });
 });
 
 events.on(eventTarget, "result-ready", (details, results) => {
-    console.log("result-ready "+ details.url +", isVulnerable: " + retire.isVulnerable(results));
-    if (retire.isVulnerable(results)) {
-        vulnerable[details.url] = results;
-        var rmsg = [];
-        for (var i in results) {
-            rmsg = rmsg.concat(results[i].vulnerabilities);
-        }
-        events.emit(eventTarget, "log", details, rmsg.join(" "));
-    } else {
-        //console.log(details.url, results);
+  console.log("result-ready "+ details.url +", isVulnerable: " + retire.isVulnerable(results));
+  if (retire.isVulnerable(results)) {
+    vulnerable[details.url] = results;
+    var rmsg = [];
+    for (var i in results) {
+      rmsg = rmsg.concat(results[i].vulnerabilities);
     }
+    events.emit(eventTarget, "log-result", details, rmsg.join(" "));
+  }
 });
 
-events.on(eventTarget, "log", (details, rmsg) => {
-    var tabId = details.tabId;
-    if (!tabMap.get(tabId)) {
-        tabMap.set(tabId, {fileCount: 0});
-    }
-    tabMap.get(tabId).fileCount++;
-    if (tabId == tabs.activeTab.id) {
-        setBadgeCount(tabMap.get(tabId).fileCount);
-    }
-    windowUtil.getMostRecentBrowserWindow().gBrowser.contentWindow.console.warn("Loaded library with known vulnerability " + details.url + " See " + rmsg);
+events.on(eventTarget, "log-result", (details, rmsg) => {
+  var tabId = details.tabId;
+  if (!tabMap.get(tabId)) {
+    tabMap.set(tabId, {fileCount: 0});
+  }
+  tabMap.get(tabId).fileCount++;
+  if (tabId == tabs.activeTab.id) {
+    setBadgeCount(tabMap.get(tabId).fileCount);
+  }
+  //Could be sent to a pageWorker instead.
+  windowUtil.getMostRecentBrowserWindow().gBrowser.contentWindow.console.warn("Loaded library with known vulnerability " + details.url + " See " + rmsg);
 });
 
 function scan(httpEvent) {
-    try {   
-        var channel = httpEvent.subject.QueryInterface(Ci.nsIHttpChannel);
-        var contentType = channel.getResponseHeader("Content-Type");
-        var url = httpEvent.subject.URI.spec;
-        if (/javascript/.test(contentType)) {
-            console.log("prepeare: " + url);
-            var tabIdForRequest = tabUtil.getTabForContentWindow(getWindowForRequest(httpEvent.subject)).getAttribute("linkedpanel").replace(/panel/, "");
-            var details = {
-                url: url,
-                tabId: tabIdForRequest
-            };
-            events.emit(eventTarget, "scan", details);
-        }
-    } catch(e) {
+  try {   
+    var channel = httpEvent.subject.QueryInterface(Ci.nsIHttpChannel);
+    var url = httpEvent.subject.URI.spec;
+    var tabIdForRequest = tabUtil.getTabForContentWindow(getWindowForRequest(httpEvent.subject)).getAttribute("linkedpanel").replace(/panel/, "");
+    if (isChannelInitialDocument(channel)) {
+      tabMap.set(tabIdForRequest, {fileCount: 0});
     }
-    return;
+    if (/javascript/.test(channel.getResponseHeader("Content-Type"))) {
+      var details = {
+        url: url,
+        tabId: tabIdForRequest
+      };
+      events.emit(eventTarget, "scan", details);
+    }
+  } catch(e) { 
+  }
+  return;
 }
 
 downloadRepo().then(function () {
-    systemEvents.on("http-on-examine-response", scan);
-    systemEvents.on("http-on-examine-cached-response", scan);
+  systemEvents.on("http-on-examine-response", scan);
+  systemEvents.on("http-on-examine-cached-response", scan);
 });
