a58060255efe472baa46584f0b8049ad2354e48b
Erlend Oftedal <erlend@oftedal.no>
2014-10-29 19:15:05 +0100

    Adding bom to chrome plugin script

--- build_chrome.sh
+++ build_chrome.sh
@@ -3,9 +3,9 @@
 FILEPATH=$(dirname $0)/chrome/js/retire.js
 
 echo "Building $FILEPATH ..."
-
-echo "var retire = (function(){" > $FILEPATH
+echo "\xef\xbb\xbfvar retire = (function(){" > $FILEPATH
 cat node/lib/retire.js >> $FILEPATH
 echo "return exports; })();" >> $FILEPATH
+
 
 echo "Done!"
