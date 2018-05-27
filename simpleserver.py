7d914f03dfb3cbc38993942a5fb65c090ef7cb49
Lilly Chalupowski <lchalupowski@gosecure.ca>
2018-05-27 20:49:14 -0300

    simpleserver python 2 and 3

--- simpleserver.py
+++ simpleserver.py
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/bin/env bash
+
+PYTHON_VERSION=$(python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))')
+
+if [ -z "$1" ]; then
+    PORT=8000
+else
+    PORT=$1
+fi
+
 cd repository
-python -m SimpleHTTPServer
+
+if [ ${PYTHON_VERSION} -eq 0 ]; then
+    python -m http.server $PORT
+else
+    python -m SimpleHTTPServer $PORT
+fi
