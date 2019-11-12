389f73c2a2403699080b1c50d429cbf0679b6c03
Jonas Krat <jkr@uni-tel.dk>
2019-11-12 09:25:14 +0100

    This is the edits to bring yarn for retirejs
    Pull request that never got into the apparently dead project https://github.com/RetireJS/retire.js/pull/253

--- node/README.md
+++ node/README.md
@@ -37,13 +37,14 @@ Options:
   --ignorefile <path>      Custom ignore file, defaults to .retireignore / .retireignore.json
   --severity <level>       Specify the bug severity level from which the process fails. Allowed levels none, low, medium, high, critical. Default: none
   --exitwith <code>        Custom exit code (default: 13) when vulnerabilities are found
+	--lockfile <type>        Use lockfile instead of package scan. Allowed types npm, yarn, yarn-workspace
   --colors                 Enable color output (console output only)
 ````
 
 The `depcheck` output format mimics the output of OWASP Dependency Check, but lacks some information compared to OWASP Dependency Check, because that information is not in the repo.
 The `cyclonedx` output format is based on based on the https://github.com/CycloneDX spec.
 
-.retireignore 
+.retireignore
 -------------
 ````
 @qs                                                             # ignore this module regardless of location
@@ -55,12 +56,12 @@ Due to a bug in ignore resolving, please upgrade to >= 1.1.3
 ------------------
 ````
 [
-	{ 
+	{
 		"component": "jquery",
 		"identifiers" : { "issue": "2432"},
 		"justification" : "We dont call external resources with jQuery"
 	},
-	{ 
+	{
 		"component": "jquery",
 		"version" : "2.1.4",
 		"justification" : "We dont call external resources with jQuery"
--- node/lib/resolve.js
+++ node/lib/resolve.js
@@ -2,6 +2,8 @@
 
 var walkdir			= require('walkdir'),
 	fs						= require('fs'),
+	findYarnWorkspaceRoot = require('find-yarn-workspace-root'),
+	lockfile			= require('@yarnpkg/lockfile'),
 	readInstalled	= require('read-installed'),
 	emitter				= require('events').EventEmitter;
 
@@ -9,7 +11,7 @@ var walkdir			= require('walkdir'),
 function listdep(parent, dep, level, deps) {
 	var stack = [];
 	var dedup = {};
-	stack.push({parent: parent, dep: dep, level: level}); 
+	stack.push({parent: parent, dep: dep, level: level});
 	while (typeof (o = stack.pop()) !== 'undefined') {
 		for (var i in o.dep.dependencies) {
 			cyclic = false;
@@ -28,42 +30,106 @@ function listdep(parent, dep, level, deps) {
 			var id = i + "@" + o.dep.dependencies[i].version;
 			if (dedup[id]) continue;
 			dedup[id] = true;
-			var d = { 
+			var d = {
 				module: { component: i, version: o.dep.dependencies[i].version }
 			};
 			if (o.dep.dependencies[i].path) {
-				d.file = "node_modules" + o.dep.dependencies[i].path.split("node_modules").slice(1).join("node_modules") + '/package.json';
+				d.file = "node_modules" + o.dep.dependencies[i].path.split("node_modules").slice(1).join("node_modules") + '/package.json'
 			}
 			deps.push(d);
-			stack.push({parent: d, dep: o.dep.dependencies[i], level: o.level + 1}); 
+			stack.push({parent: d, dep: o.dep.dependencies[i], level: o.level + 1});
 		}
 	}
 }
 
-function getNodeDependencies(path, limit) {
-	var events = new emitter();
+function fetchChildDependencies(path, limit, events) {
 	readInstalled(path, {}, function (er, pkginfo) {
 		var deps = [];
 		if (limit) {
 			var packages = JSON.parse(fs.readFileSync(path +'/package.json'));
-			filter = [];			
+			filter = [];
 
 			var filter = packages.dependencies ? Object.keys(packages.dependencies) : [];
-
 			Object.keys(pkginfo.dependencies)
-				.filter(function(d) { return !pkginfo.dependencies[d]._requiredBy || pkginfo.dependencies[d]._requiredBy.indexOf("/") > -1 || pkginfo.dependencies[d]._requiredBy.indexOf("#DEV:/") > -1;  })
+				.filter(function(d) { return !pkginfo.dependencies[d]._requiredBy || pkginfo.dependencies[d]._requiredBy.indexOf("/") > -1;  })
 				.filter(function(d) { return filter.indexOf(d) == -1; })
 				.forEach(function(d) { delete pkginfo.dependencies[d]; });
 		}
-		var notInstalled = Object.keys(pkginfo.dependencies).filter(function (d) {
-			return !pkginfo.dependencies[d].path;
-		});
+		var notInstalled = Object.keys(pkginfo.dependencies).filter(d => !pkginfo.dependencies[d].path);
 		if (notInstalled.length > 0) {
 			return events.emit('error', 'Could not find dependencies: ' + notInstalled.join(', ') + '. You may need to run npm install');
 		}
+
 		listdep({file: 'package.json',component: pkginfo.name, version: pkginfo.version}, pkginfo, 1, deps);
-		events.emit('done', deps);				
+		events.emit('done', deps);
 	});
+}
+
+function fetchYarnLockDependencies(lockFileName, events) {
+	if (!fs.existsSync(lockFileName)) {
+		return events.emit('error', 'Could not find lockfile ' + lockFileName);
+	}
+
+	var file = fs.readFileSync(lockFileName, 'utf8');
+	var json = lockfile.parse(file);
+
+	if (json.type !== 'success') {
+		return events.emit('error', 'Could not parse lockfile ' + lockFileName);
+	}
+
+	var result = json.object;
+	var deps = Object.keys(result).map(function (key) {
+		return {
+			module: {
+				component: key.split('@')[0],
+				version: result[key].version
+			},
+		};
+	});
+
+	events.emit('done', deps);
+}
+
+function fetchPackageLockDependencies(lockFileName, events) {
+	if (!fs.existsSync(lockFileName)) {
+		return events.emit('error', 'Could not find lockfile ' + lockFileName);
+	}
+
+	var file = fs.readFileSync(lockFileName, 'utf8');
+	var json = JSON.parse(file);
+
+	var result = json.dependencies;
+	var deps = Object.keys(result).map(function (key) {
+		return {
+			module: {
+				component: key,
+				version: result[key].version
+			},
+		};
+	});
+
+	events.emit('done', deps);
+}
+
+function getNodeDependencies(path, limit, lockfile) {
+	var events = new emitter();
+
+	if (lockfile !== undefined) {
+		if (lockfile === 'yarn') {
+			fetchYarnLockDependencies('./yarn.lock', events);
+		} else if (lockfile === 'yarn-workspace') {
+			var yarnWorkspaceRoot = findYarnWorkspaceRoot();
+			var lockFileName = yarnWorkspaceRoot + '/yarn.lock';
+			fetchYarnLockDependencies(lockFileName, events);
+		} else if (lockfile === 'npm') {
+			fetchPackageLockDependencies(path + '/package-lock.json', events);
+		} else {
+			events.emit('error', 'Invalid lockfile type ' + lockfile);
+		}
+	} else {
+		fetchChildDependencies(path, limit, events);
+	}
+
 	return events;
 }
 
@@ -84,7 +150,6 @@ exports.scanJsFiles = function(path) {
 	return scanJsFiles(path);
 };
 
-exports.getNodeDependencies = function(path, limit) {
-	return getNodeDependencies(path, limit);
+exports.getNodeDependencies = function(path, limit, lockfile) {
+	return getNodeDependencies(path, limit, lockfile);
 };
-
