9c453a8d6ee0aa64578022957602da2f0beed8d0
Erlend Oftedal <erlend@oftedal.no>
2023-06-01 20:17:41 +0200

    Add test for purl

--- node/spec/tests/cyclonedx.spec.js
+++ node/spec/tests/cyclonedx.spec.js
@@ -1,61 +1,60 @@
 /*jshint esversion: 8 */
 
-var jsonLogger = require("../../lib/reporters/cyclonedx-json").default;
-var xmlLogger = require("../../lib/reporters/cyclonedx").default;
-var fs = require("fs");
+var jsonLogger = require('../../lib/reporters/cyclonedx-json').default;
+var xmlLogger = require('../../lib/reporters/cyclonedx').default;
+var fs = require('fs');
 var Validator = require('jsonschema').Validator;
-var retire = require("../../lib/retire");
+var retire = require('../../lib/retire');
 
-var reporting = require("../../lib/reporting");
+var reporting = require('../../lib/reporting');
 var hash = reporting.hash;
-var repo = require("../repository.json");
-var libxmljs = require("libxmljs");
+var repo = require('../repository.json');
+var libxmljs = require('libxmljs');
 
-let jsonSchema = require("../schema/bom-1.4.schema.json");
-let jsfSchema = require("../schema/jsf-0.82.schema.json");
-const { fail } = require("assert");
+let jsonSchema = require('../schema/bom-1.4.schema.json');
+let jsfSchema = require('../schema/jsf-0.82.schema.json');
+const { fail } = require('assert');
 
 describe('cyclonedx-json', () => {
+  it('should validate report according to schema', () => {
+    let data = [];
+    let writer = {
+      out: (a) => data.push(a),
+      err: (a) => data.push(a),
+      close: () => undefined,
+    };
+    let logger = reporting.open({});
+    jsonLogger.configure(logger, writer, {}, hash);
+    let result1 = retire.scanFileContent('/*! jQuery v1.8.1 asdasd ', repo, hash);
+    logger.logVulnerableDependency({ results: result1 });
+    logger.close();
+    let validator = new Validator();
+    validator.addSchema(jsfSchema, 'jsf-0.82.schema.json#/definitions/signature');
+    let output = JSON.parse(data.join(''));
+    data.join('').should.contain('pkg:npm/jquery@1.8.1');
+    let res = validator.validate(output, jsonSchema);
+    res.valid.should.equal(true);
+  });
 
-    it("should validate report according to schema", () => {
-        let data = [];
-        let writer = {
-            out: (a) => data.push(a),
-            err: (a) => data.push(a),
-            close: () => {}
-        };
-        let logger = reporting.open({});
-        jsonLogger.configure(logger, writer, {}, hash);
-        let result = retire.scanFileContent("/*! jQuery v1.8.1 asdasd ", repo, hash);
-        logger.logVulnerableDependency({ results: result });
-        logger.close();
-        let validator = new Validator();
-        validator.addSchema(jsfSchema, "jsf-0.82.schema.json#/definitions/signature");
-        let output = JSON.parse(data.join(""));
-        let res = validator.validate(output, jsonSchema);
-        res.valid.should.equal(true);
-    });
-
-
-    it("should validate report according to xml schema", () => {
-        let data = [];
-        let writer = {
-            out: (a) => data.push(a),
-            err: (a) => data.push(a),
-            close: () => {}
-        };
-        let logger = reporting.open({});
-        xmlLogger.configure(logger, writer, {}, hash);
-            let result = retire.scanFileContent("/*! jQuery v1.8.1 asdasd ", repo, hash);
-        logger.logVulnerableDependency(result);
-        logger.close();
-        let xml = data.join("");
-        let xmlDoc = libxmljs.parseXml(xml);
-        let schemaS = fs.readFileSync("spec/schema/bom-1.4.xsd", "utf-8");
-        let schema = libxmljs.parseXml(schemaS, { baseUrl: "spec/schema/" });
-        let res = xmlDoc.validate(schema);
-        if (!res) {
-            fail(xmlDoc.validationErrors);
-        }
-    });
+  it('should validate report according to xml schema', () => {
+    let data = [];
+    let writer = {
+      out: (a) => data.push(a),
+      err: (a) => data.push(a),
+      close: () => undefined,
+    };
+    let logger = reporting.open({});
+    xmlLogger.configure(logger, writer, {}, hash);
+    let result = retire.scanFileContent('/*! jQuery v1.8.1 asdasd ', repo, hash);
+    logger.logVulnerableDependency(result);
+    logger.close();
+    let xml = data.join('');
+    let xmlDoc = libxmljs.parseXml(xml);
+    let schemaS = fs.readFileSync('spec/schema/bom-1.4.xsd', 'utf-8');
+    let schema = libxmljs.parseXml(schemaS, { baseUrl: 'spec/schema/' });
+    let res = xmlDoc.validate(schema);
+    if (!res) {
+      fail(xmlDoc.validationErrors);
+    }
+  });
 });
