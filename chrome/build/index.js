3b01791495da8976bd652843d95ca9a693b24825
Erlend Oftedal <erlend@oftedal.no>
2025-12-06 09:36:39 +0100

    Update extensions with new repo

--- chrome/build/index.js
+++ chrome/build/index.js
@@ -1,5 +1,5 @@
 const deepScan = require("../../node/lib/deepscan.js").deepScan;
 const retire = require("../../node/lib/retire.js");
-exports.repo = require("../../repository/jsrepository-v3.json");
+exports.repo = require("../../repository/jsrepository-v5.json");
 exports.retire = retire;
 exports.deepScan = deepScan;
--- chrome/extension/js/background.js
+++ chrome/extension/js/background.js
@@ -1,7 +1,7 @@
 /* global chrome, console, exports, CryptoJS, Emitter */
 
 const repoUrl =
-  "https://raw.githubusercontent.com/RetireJS/retire.js/master/repository/jsrepository-v3.json";
+  "https://raw.githubusercontent.com/RetireJS/retire.js/master/repository/jsrepository-v5.json";
 let updatedAt = Date.now();
 let repo;
 let backdoorData;
